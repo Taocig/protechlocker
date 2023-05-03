d709836155d19f25438e00a2c74ef8d0f2e62e76
Taocig <114158192+Taocig@users.noreply.github.com>
2023-05-03 10:46:42 +0200

    Delete include directory

--- include/footer.inc.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <div class="content-footer">
-        <a href="#">Mentions l&eacute;gales</a>
-        <a href="#">Politique de confidentialit&eacute;</a>
-        <a href="#">Coordonn&eacute;es</a>
-    </div>
-</footer>
