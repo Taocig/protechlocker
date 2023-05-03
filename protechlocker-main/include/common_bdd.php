e96982a58bf9ad0f08401e01df9dd2108323a127
Taocig <114158192+Taocig@users.noreply.github.com>
2023-05-03 10:46:54 +0200

    Delete protechlocker-main directory

--- protechlocker-main/include/common_bdd.php
+++
@@ -1,3 +0,0 @@
-<?php
-//$bdd = new PDO('mysql:host=localhost;dbname=protechlocker;charset=utf8','root','root');
-?>
--- protechlocker-main/include/header.inc.php
+++
@@ -1,9 +0,0 @@
-<div class="box">
-        <iframe width="425" height="344"
-        src="https://www.youtube-nocookie.com/embed/Je0CtTUpijQ?autoplay=1&mute=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen ></iframe>
-    <div class="logo">
-        <div>PROTECT</div>
-        <div>LOCKER</div>
-        <div>TECHNOLOGY</div>
-    </div>  
-</div>
